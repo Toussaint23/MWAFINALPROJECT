b32b7c7f0cc49f8425d8f8a719f684990b87a158
Mengistu Bogale <mengistuy@gmail.com>
2018-07-14 16:10:11 -0500

    dbConnection

--- careercompanionserver/db_connection/database_connection.js
+++ careercompanionserver/db_connection/database_connection.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('./config/properties.file');
-var connection=properties.get('server.connection');
+var connection=properties.get('db.connection');
 
    var con=mongoose.connect(connection, {useNewUrlParser:true})
   .then(() =>  console.log('connection successful'))
